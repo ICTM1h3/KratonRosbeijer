f0a8375d49cb008b9853bed09a61dcc6a01bf2cf
Niels Breuker <niels.breuker@hotmail.nl>
2017-11-21 10:43:52 +0100

    Added better comments

--- database.php
+++ database.php
@@ -1,19 +1,21 @@
 
 
 <?php
-
 $db = "mysql:host=localhost; dbname=winkel; port=3306";
 $user = "root";
 $pass = "";
 $db = new PDO($db, $user, $pass);
 
 //Function for execute a query
-function base_query($query, $params= NULL){
+function base_query($query, $params = NULL) {
+    // Grab the global $db object.
     global $db;
+    // Tell PDO to prepare the query. We'll provide the parameters later on.
     $stmt = $db->prepare($query);
+
+    // Execute the prepared query with the provided parameters.
     $stmt->execute($params);
     return $stmt;
-
 }
 
 
--- pages/homepage.php
+++ pages/homepage.php
@@ -1,5 +1,5 @@
 <?php
-setTitle("Homepage <b>");
+setTitle("Homepage");
 
 ?>
 
