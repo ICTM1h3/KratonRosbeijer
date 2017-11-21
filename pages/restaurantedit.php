1bb7507e22af24b34270efb1b196a59dbf974cf0
Maartenzand <maartenzand300@gmail.com>
2017-11-21 12:27:02 +0100

    First setup for restauranteditplugin

---
+++ pages/restaurantedit.php
@@ -0,0 +1,15 @@
+<?php
+setTitle("Beheren restaurant informatie");
+?>
+
+<h1>Beheren restaurant informatie</h1>
+
+Op deze pagina kan de informatie van de informatie pagina worden aangepast. Dit kan door middel van de edit plugin.  
+
+<textarea class = "tinymce">Hier komt de tekst van de databas, gevuld met de informatie voor de informatie pagina</textarea>
+
+
+		<!-- javascript -->
+		<script type="text/javascript" src="restauranteditplugin/js/jquery.min.js"></script>
+		<script type="text/javascript" src="restauranteditplugin/plugin/tinymce/js/tinymce/tinymce.min.js"></script>
+		<script type="text/javascript" src="restauranteditplugin/plugin/tinymce/js/tinymce/init-tinymce.js"></script>
