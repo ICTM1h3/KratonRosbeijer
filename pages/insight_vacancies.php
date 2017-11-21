a2e0b1109f8eaad51e3a102077bc7425a9168530
RiseM1804 <33448680+RiseM1804@users.noreply.github.com>
2017-11-21 15:25:20 +0100

    Verbeterd

--- pages/insight_vacancies.php
+++ pages/insight_vacancies.php
@@ -4,39 +4,56 @@
 setTitle("Vacancies");
 // Fetching all data that is required for vacancies
 $query = base_query('SELECT * FROM Vacancy')->fetchAll();
-
 ?>
 <p>General information like how to contact the employer</p>
 <?php
 // Storing the data in variables
 foreach ($query as $value) {
-    $class = $value['Id'];
     $title = $value['Title'];
     $function = $value['Function'];
     $description = $value['Description'];
     $employment = $value['Employment'];
+    // If the Id number is odd the element will float on the left
+    if ($value['Id'] % 2 != 0) {
+        $class = " style='float:left;'";
+    } else {
+        $class = " style='float:center;'";
+    }
     ?>
-    <table id="<?= $class ?>">
+    <table <?= $class ?>
         <tr>
+            <td style="font-weight:bold;">
+                 Titel: 
+            </td>
             <td>
-                <?= $title ?> 
+                <?= $title ?>
             </td>
         </tr>
         <tr>
+            <td style="font-weight:bold;">
+                 Functie categorie: 
+            </td>
             <td> 
                 <?= $function ?> 
             </td>
         </tr>
         <tr>
+            <td style="font-weight:bold;">
+                 Beschrijving: 
+            </td>
             <td> 
                 <?= $description ?> 
             </td>
         </tr>
         <tr>
+            <td style="font-weight:bold;">
+                 Dienstverband: 
+            </td>
             <td> 
                 <?= $employment ?>s 
             </td>
         </tr>
     </table>
+    <br>
 <?php
 } 
