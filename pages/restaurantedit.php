69c16955c798591cc0391edca5b4f1f3145b593e
Maartenzand <maartenzand300@gmail.com>
2017-11-21 13:30:04 +0100

    update

--- pages/restaurantedit.php
+++ pages/restaurantedit.php
@@ -6,10 +6,20 @@ setTitle("Beheren restaurant informatie");
 
 Op deze pagina kan de informatie van de informatie pagina worden aangepast. Dit kan door middel van de edit plugin.  
 
-<textarea class = "tinymce">Hier komt de tekst van de databas, gevuld met de informatie voor de informatie pagina</textarea>
 
+
+        <form id="get-data-form" method="post">
+
+			<textarea class="tinymce" id="texteditor">DE TEKST</textarea>
+			<input type="submit" value="Get Data">
+
+		</form>
+
+		<div id="data-container">
+		</div>
 
 		<!-- javascript -->
-		<script type="text/javascript" src="restauranteditplugin/js/jquery.min.js"></script>
-		<script type="text/javascript" src="restauranteditplugin/plugin/tinymce/js/tinymce/tinymce.min.js"></script>
-		<script type="text/javascript" src="restauranteditplugin/plugin/tinymce/js/tinymce/init-tinymce.js"></script>
+		<script type="text/javascript" src="editplugin/js/jquery.min.js"></script>
+		<script type="text/javascript" src="editplugin/plugin/tinymce/js/tinymce/tinymce.min.js"></script>
+		<script type="text/javascript" src="editplugin/plugin/tinymce/js/tinymce/init-tinymce.js"></script>
+        <script type="text/javascript" src="editplugin/js/getdata.js"></script>
