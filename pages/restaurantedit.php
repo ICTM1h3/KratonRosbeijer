100183678d195aee20680d6b736bca14e74f15fd
Maartenzand <maartenzand300@gmail.com>
2017-11-21 14:30:03 +0100

    LAST UDPATE OF THE DAY

--- pages/restaurantedit.php
+++ pages/restaurantedit.php
@@ -1,24 +1,49 @@
 <?php
 setTitle("Beheren restaurant informatie");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$text = file_get_contents('php://input');
+	base_query("UPDATE setting SET value = :value WHERE name = 'Info'", array(':value' => $text));
+}
 ?>
 
-<h1>Beheren restaurant informatie</h1>
+Beheren restaurant informatie
 
 Op deze pagina kan de informatie van de informatie pagina worden aangepast. Dit kan door middel van de edit plugin.  
 
 
+		<!--Making form for the edit-plugin-->
 
-        <form id="get-data-form" method="post">
 
-			<textarea class="tinymce" id="texteditor">DE TEKST</textarea>
-			<input type="submit" value="Get Data">
+			<textarea class="tinymce" id="texteditor">
 
-		</form>
+			<!-- Loading the info page into the editor -->
+			<?php $info = base_query("SELECT * FROM setting WHERE name = 'Info' ")->fetch();
+      		echo($info['Value']);
+   			 //used the function base_query to execute a query and echo's the result 
+ 			?> 
 
+			</textarea>
+			<input onclick="submit()" type="submit" value="Get Data">
+
+			<script>
+
+			//Submit the data
+			function submit(){
+
+				$.ajax({
+					url: '?p=restaurantedit', // Url witch deals the request
+					data: tinyMCE.activeEditor.getContent(),
+					method: "POST"
+				});
+			}
+			</script>
+
+		<!-- Put the data from Get Data here, temporarily-->
 		<div id="data-container">
 		</div>
 
-		<!-- javascript -->
+		<!-- Loading javascript for the edit tool -->
 		<script type="text/javascript" src="editplugin/js/jquery.min.js"></script>
 		<script type="text/javascript" src="editplugin/plugin/tinymce/js/tinymce/tinymce.min.js"></script>
 		<script type="text/javascript" src="editplugin/plugin/tinymce/js/tinymce/init-tinymce.js"></script>
