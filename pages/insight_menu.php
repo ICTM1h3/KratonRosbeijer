e742f0f8072201da8122ee4ebff6198773841a4d
RiseM1804 <33448680+RiseM1804@users.noreply.github.com>
2017-11-28 14:43:20 +0100

    Fix?

--- pages/insight_menu.php
+++ pages/insight_menu.php
@@ -1,26 +1,3 @@
-
-<style>
-
-ul {
-    list-style: none;
-}
-
-* {
-    font-family: Arial;
-}
-
-#categoryPrice {
-    float:right;
-    font-weight: normal;
-    font-size:17px;
-}
-
-#price {
-    float:right;
-}
-
-</style>
-
 <?php
 
 function echoCategory($categoryId, $size = 1) {
@@ -45,28 +22,18 @@ function echoCategory($categoryId, $size = 1) {
 <?php
 
 // If there are still subcategories the function will keep being called upon
-$dishes = base_query("SELECT * FROM Dish WHERE Category = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
-    if ($subcategories == true) {?>
-        <ul> <?php
+    $dishes = base_query("SELECT * FROM Dish WHERE Category = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
+    foreach($dishes as $dishValue){ ?>
+       <ul> <?php
         foreach ($dishes as $dishValue)
         {
             ?><li>- <?= $dishValue['Name']?><span id="price"><?= $dishValue['Price'] ?></span><?= "<br>" . "" . $dishValue['Description']?><?php
         }
-    ?> </ul><?php
+    ?> </ul>
+    <?php
         foreach ($subcategories as $category) {
             echoCategory($category['Id'], $size + 1);
         }
-    }
-
-    // If there are no subcategories anymore the function will echo all the dishes attached to the category
-    else {
-?>  <ul> <?php
-foreach ($dishes as $dishValue)
-{
-    ?><li>- <?= $dishValue['Name']?><span id="price"><?= $dishValue['Price'] ?></span><?= "<br>" . "" . $dishValue['Description']?><?php
-}
-?> </ul> 
-<?php 
     }
 }
 
@@ -75,3 +42,26 @@ $mainCategories = base_query("SELECT * FROM DishCategory WHERE ParentCategoryId 
 foreach ($mainCategories as $category) {
     echoCategory($category['Id']);
 }
+?>
+
+<style>
+
+ul {
+    list-style: none;
+}
+
+* {
+    font-family: Arial;
+}
+
+#categoryPrice {
+    float:right;
+    font-weight: normal;
+    font-size:17px;
+}
+
+#price {
+    float:right;
+}
+
+</style>
