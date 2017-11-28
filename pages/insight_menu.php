2bdcadc7bcd9f05ef70da1571ebb48973ca582fa
RiseM1804 <33448680+RiseM1804@users.noreply.github.com>
2017-11-28 15:42:50 +0100

    uhuh

--- pages/insight_menu.php
+++ pages/insight_menu.php
@@ -3,7 +3,7 @@
 function echoCategory($categoryId, $size = 1) {
     $subcategories = base_query("SELECT * FROM DishCategory WHERE ParentCategoryId = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
     $category = base_query("SELECT * FROM DishCategory WHERE Id = :categoryId", [':categoryId' => $categoryId])->fetch();
-?>
+    ?>
 
 <!-- Echo category name -->
     <h<?= $size ?>>
@@ -21,21 +21,19 @@ function echoCategory($categoryId, $size = 1) {
 
 <?php
 
-// If there are still subcategories the function will keep being called upon
+    // If there are still subcategories the function will keep being called upon
     $dishes = base_query("SELECT * FROM Dish WHERE Category = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
+    // var_dump($dishes);
     foreach($dishes as $dishValue){ ?>
        <ul> <?php
-        foreach ($dishes as $dishValue)
-        {
-            ?><li>- <?= $dishValue['Name']?><span id="price"><?= $dishValue['Price'] ?></span><?= "<br>" . "" . $dishValue['Description']?><?php
-        }
-    ?> </ul>
+            ?><li><span id="dishTitle"><?= $dishValue['Name']?></span><span id="price"><?= $dishValue['Price'] ?></span><?= "<br>" . "" . $dishValue['Description']?><?php
+        ?> </ul>
     <?php
+    }
         foreach ($subcategories as $category) {
             echoCategory($category['Id'], $size + 1);
         }
     }
-}
 
 // Calling upon the function with 'Headcategories'
 $mainCategories = base_query("SELECT * FROM DishCategory WHERE ParentCategoryId IS NULL ORDER BY Position")->fetchAll();
@@ -52,6 +50,10 @@ ul {
 
 * {
     font-family: Arial;
+}
+
+#dishTitle {
+    font-weight: bold;
 }
 
 #categoryPrice {
