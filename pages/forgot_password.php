e0dd3d660eacd3ba9216bcfb377d60890d40cf1d
Iris-Christiaanse <christiaanse.iris@gmail.com>
2017-11-30 12:21:40 +0100

    continue reset password

--- pages/forgot_password.php
+++ pages/forgot_password.php
@@ -5,11 +5,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $forget = base_query("SELECT * FROM User WHERE Email=:email", array(
             ":email" => $_POST["email"]
     ))->fetch();
-    var_dump($forget);
+    //var_dump($forget);
     if ($_POST["email"] == ""){
         echo ("Vul een email in."); 
     } elseif($_POST["email"] == $forget["Email"]) {
-            mail("christiaanse.iris@gmail.com", "Wachtwoord vergeten", "Geachte Gast, \n Door op de volgende link te klikken kunt u een nieuw wachtwoord aanmaken:\n dit is een linkje whoop \n met vriendelijke groet, \n Iets");
+            mail("christiaanse.iris@gmail.com", "Wachtwoord vergeten", "Geachte Gast, \n Door op de volgende link te klikken kunt u een nieuw wachtwoord aanmaken:\n http://localhost/kratonrosbeijer/?p=reset_password \n met vriendelijke groet, \n Iets");
             echo ("Er is een email verzonden.");
     } else {
         echo ("Onbekend emailadres.");
--- pages/reset_password.php
+++ pages/reset_password.php
@@ -1,20 +1,28 @@
 <?php
-session_start();
 
 //function to change the page name
 setTitle("Wachtwoord veranderen");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $reset = base_query("UPDATE user
-    SET Password
-    ");
+    $reset = base_query("UPDATE User SET Password WHERE Password=:password" , array(":password" => $_POST["password1"]
+    ))->fetch();
+    if ($_POST["password1"] == $_POST["password2"]){
+        password_hash($_POST["password1"]);
+        echo("uw wachtwoord is reset.");
+    } elseif ($_POST["password1"] || $_POST["password2"] == ""){
+        echo("Vul beide velden in om uw wachtwoord te resetten.");
+    } elseif ($_POST["password1"] != $_POST["password2"]){
+        echo ("Wachtwoord is niet gelijk");
+    }
     //continue query, need to verify new password, new password set in database
 }
 ?>
 
 <form method="post" class="container">
     <h2>Nieuw Wachtwoord</h2>
-    <div>
-        <label>Nieuw Wachtwoord</label>
-        <input type="password" name="password1" placeholder="Wachtwoord">
-        <input type="password" name="password2" placeholder="Herhaal Wachtwoord">
+        <div class="form-group">
+            <label>Nieuw Wachtwoord</label></br>
+            <input type="password" name="password1" placeholder="Wachtwoord"></br>
+            <input type="password" name="password2" placeholder="Herhaal Wachtwoord"></br>
+        </div>
+        <button class="btn btn-primary" type="submit" name="submit">RESET</button>
