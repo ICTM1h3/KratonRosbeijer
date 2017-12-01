9eab91d0346256eccdb84fac4bda03676b619b80
RiseM1804 <33448680+RiseM1804@users.noreply.github.com>
2017-12-01 09:48:31 +0100

    Yes

--- pages/insight_menu.php
+++ pages/insight_menu.php
@@ -1,46 +1,55 @@
 <?php
 
-function echoCategory($categoryId, $size = 1) {
+function echoCategory($categoryId, $size = 1) 
+{
     $subcategories = base_query("SELECT * FROM DishCategory WHERE ParentCategoryId = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
     $category = base_query("SELECT * FROM DishCategory WHERE Id = :categoryId", [':categoryId' => $categoryId])->fetch();
-    ?>
-
-<!-- Echo category name -->
-<div style="margin:<?= $size * 10 ?>px">
-    <h<?= $size ?>>
-        <?= $category['Name']?>
-        <!-- Checks if a category has a price attached to itself -->
-        <?php if(isset($category['Price'])) {
-            ?><span id='categoryPrice'><?= $category['Price'] ?></span><?php
-        } ?>
-    </h<?= $size ?>> 
-
-<!-- Echo category description -->
-     <i><p>
-        <?= $category['Description'] ?>
-    </p></i> 
-
-<?php
-
-    // If there are still subcategories the function will keep being called upon
     $dishes = base_query("SELECT * FROM Dish WHERE Category = :categoryId ORDER BY Position", [':categoryId' => $categoryId])->fetchAll();
-    // var_dump($dishes);
-    foreach($dishes as $dishValue){ ?>
-        <ul>
-            <li><span id="dishTitle"><?= $dishValue['Name']?></span><span id="price"><?= $dishValue['Price'] ?></span><?= "<br>" . "" . $dishValue['Description']?>
-        </ul>
+    
+    ?>
+    <!-- Echo category name -->
+    <div style="margin:<?= $size * 10 ?>px">
+        <h<?= $size ?>>
+            <?= $category['Name']?>
+            <!-- Checks if a category has a price attached to itself -->
+            <?php if(isset($category['Price'])) {
+                ?><span id='categoryPrice'><?= $category['Price'] ?></span><?php
+            } ?>
+        </h<?= $size ?>> 
+
+    <!-- Echo category description -->
+        <i><p>
+            <?= $category['Description'] ?>
+        </p></i> 
+
     <?php
-    }
-        foreach ($subcategories as $category) {
+
+        foreach($dishes as $dishValue)
+        { ?>
+            <ul>
+        <li><span id="dishTitle"><?= $dishValue['Name']?></span><span id="price"><?php if($dishValue['Price'] != 0.00){?><?= $dishValue['Price']?><?php }?></span><?= "<br>" . "" . $dishValue['Description']?>
+            </ul>
+        <?php
+        }
+        foreach ($subcategories as $category) 
+        {
             echoCategory($category['Id'], $size + 1);
         }
-    ?></div><?php
+        ?></div><?php
     }
 
 // Calling upon the function with 'Headcategories'
 $mainCategories = base_query("SELECT * FROM DishCategory WHERE ParentCategoryId IS NULL ORDER BY Position")->fetchAll();
-foreach ($mainCategories as $category) {
-    echoCategory($category['Id']);
+if(empty($mainCategories))
+{
+    echo ("Er zijn geen gerechten op dit moment");
+}
+else
+{
+    foreach ($mainCategories as $category) 
+        {
+            echoCategory($category['Id']);
+        }
 }
 ?>
 
