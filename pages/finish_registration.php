75a6aec9e3b6ded6d2acb1211fdebb71d6133081
Niels Breuker <niels.breuker@hotmail.nl>
2017-12-06 11:43:17 +0100

    Provide a message in finish_registration if the registrationcode was not found

--- pages/finish_registration.php
+++ pages/finish_registration.php
@@ -6,11 +6,15 @@ if (!isset($_GET['registrationCode'])) {
 }
 
 // Remove the registrationcode from the user.
-base_query("UPDATE User 
+$stmt = base_query("UPDATE User 
 SET RegistrationCode = NULL
 WHERE RegistrationCode = :registrationCode", [
     ":registrationCode" => $_GET['registrationCode'],
 ]);
 
-?>
-<h2>U bent geregistreerd</h2>
+
+if ($stmt->rowCount() == 1) { ?>
+    <h2>U bent geregistreerd</h2>
+<?php } else { ?>
+    <h2>De activatiecode klopt of bestaat niet meer.</h2>
+<?php } ?>
