1bd2951e1862acd636afa716ba3244b3c0649601
Maartenzand <maartenzand300@gmail.com>
2017-12-15 14:04:00 +0100

    update

--- pages/3_manage_giftcard.php
+++ pages/3_manage_giftcard.php
@@ -1,33 +1,35 @@
 <?php
 setTitle("Beheer cadeaubonnen");
 
-$code = base_query("SELECT CouponCode FROM coupon");
+$code = base_query("SELECT CouponCode FROM coupon ORDER BY CurrentValue ");
 
 
-function str_start_with($value, $start){
-       return substr($value, 0, strlen($start)) === $start;
-} 
-
-function changeGiftCard($code){
-    echo $code;
-    if(isset($_POST['change_'][$code]))
-        echo "het is ";
-        base_query("UPDATE coupon SET CurrentValue = :currentvalue WHERE CouponCode = :couponcode",[
-        ':currentvalue' => $_POST['currentvalue'],
-        ':couponcode' => $code
-    ]);
-
-
-}
 
 //Retrieve all current giftcards.
 $giftcards = base_query("SELECT * FROM coupon");
 
-foreach($_POST as $key => $value){
-    if(str_start_with($key, "change_")){
-        $coupon = substr($key, 7);
-        changeGiftCard($coupon);
-    } 
+
+
+function changeGiftCard($code, $value) {
+    if (base_query("SELECT InitialValue FROM Coupon WHERE CouponCode = :couponcode", [':couponcode' => $code])->fetchColumn() < $value) {
+       echo "fout";
+
+    }else if($value >0){
+        base_query("UPDATE coupon SET CurrentValue = :currentvalue WHERE CouponCode = :couponcode",[
+            ':currentvalue' => $value,
+            ':couponcode' => $code
+	    ]);
+    
+        header("Location: ?p=manage_giftcard");
+    }
+}
+
+
+if (isset($_POST['changecoupon'])) {
+	foreach ($_POST['changecoupon'] as $couponCode => $value) {
+		changeGiftCard($couponCode, $_POST['currentvalue'][$couponCode]);
+    }
+
 }
 
 
@@ -54,19 +56,17 @@ table, tr, td {
             <td>Oorspronkelijke besteller</td>
             <td>E-mail</td>
         </tr>
-            <?php
-            foreach($giftcards as $card){
-            ?>
-        <tr>
-            <td><?= $card['CouponCode']?></td>
-            <td><?= $card['InitialValue']?></td>
-            <td><input type="text" name="currentvalue" value="<?= $card['CurrentValue']?>"/></td>
-            <td><?= $card['Email']?></td>
-            <td><?= $card['InNameOf']?></td>
-            <td><input type="submit" name="change_<?= $card['CouponCode']?>" value="Opslaan"/></td>
-        </tr>
-        </tr>
-        <?php }
+        <?php
+            foreach($giftcards as $card){ ?>
+            <tr>
+                <td><?= $card['CouponCode']?></td>
+                <td><?= $card['InitialValue']?></td>
+                <td><input type="text" name="currentvalue[<?= $card['CouponCode']?>]" value="<?= $card['CurrentValue']?>"/></td>
+                <td><?= $card['Email']?></td>
+                <td><?= $card['InNameOf']?></td>
+                <td><input type="submit" name="changecoupon[<?= $card['CouponCode']?>]" value="Opslaan"/></td>
+            </tr><?php 
+            }
         }?>
     </table>
 </form>
