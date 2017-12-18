6bb18fe49473a5c669e98fa92ec63516faf40f37
Iris-Christiaanse <christiaanse.iris@gmail.com>
2017-12-18 09:33:36 +0100

    update

--- pages/0_cateringpage.php
+++ pages/0_cateringpage.php
@@ -5,7 +5,6 @@
 ?>
 
 <div>
-<script>$('.carousel').carousel(data-interval="100")</script>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -36,9 +35,9 @@
 echo($text['Value']);
 ?>
 
-<div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-</div>
+<script>$('.carousel').carousel({interval : 100})</script>
+
 </div>
